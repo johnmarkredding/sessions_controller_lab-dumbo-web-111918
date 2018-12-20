1a51a56f2a35b443a1137acdd1eec4b5a55114b3
John Mark Redding <johnmarkredding@gmail.com>
2018-12-20 19:30:46 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,9 +7,8 @@ class SessionsController < ApplicationController
     if session[:name] == params[:name]
       session[:name] = params[:name]
       redirect_to '/'
-    else
-      redirect_to '/sessions/new'
     end
+      redirect_to '/sessions/new'
   end
   
   def destroy
